254bafe7487c01502e8b878c7a84a653325996ae
Avinash Kadimisetty <kavinash366@gmail.com>
2018-09-24 00:06:30 -0500

    Started Multinomial Naive Bayes Classifier Code

---
+++ Classification/NaiveBayes/Multinomial-Naive-Bayes/mnb_classifier.py
@@ -0,0 +1,8 @@
+# For building the Gaussioan Naive Bayes classifier, Iris dataset from Scikit Learn is being used.
+from sklearn.datasets import load_iris
+
+
+# Importing necessary scikit learn libraries
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
