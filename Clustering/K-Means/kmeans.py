fbffb3fbd013992675585079fb617d4065312022
Avinash Kadimisetty <kavinash366@gmail.com>
2018-09-30 16:30:12 -0500

    Implemented kmeans using sklearn

--- Clustering/K-Means/kmeans.py
+++ Clustering/K-Means/kmeans.py
@@ -1,3 +1,13 @@
 import numpy as np
 import pandas as pd
-from matplotlib mport pyplot as plt
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
+import pandas as pd
+
+dataset = pd.read_csv('dataset.csv')
+
+kmeans = KMeans(n_clusters = 6)
+clusters = kmeans.fit(dataset)
+cluster_groups = kmeans.predict(dataset)
+cluster_centroids = clusters.cluster_centers_
+print(cluster_centroids)
